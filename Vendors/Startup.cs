719d79ebe36ae327c57e967f8d58b2df03f01d14
Darren Owen <dazza_235@hotmail.co.uk>
2016-04-05 09:15:56 +0100

    Add project files.

---
+++ Vendors/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vendors.Startup))]
+namespace Vendors
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
